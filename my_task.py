c62983e164735f90d5da32051d148b0dcede1f30
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-02-03 23:16:41 +0200

    answer_4 is done

--- my_task.py
+++ my_task.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
+path = Path.cwd()
+files_count = 0
+folders_count = 0
 
-path = Path('documents/reports/annual_report.pdf')
+for item in path.iterdir():
+    if item.is_dir():
+        print("Folder:", item.name)
+        folders_count += 1
+    elif item.is_file():
+        print("File:", item.name)
+        files_count += 1
 
-print(path.name)
-print(path.stem)
-print(path.suffix)
-print(path.parent)
-
-for p in path.parents:
-    print(p)
-
+print("Folders count:", folders_count)
+print("Files count:", files_count)
